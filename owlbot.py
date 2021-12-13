c2ed7fd7ca5de3df65d42b61ff8e9de0c82781ba
Takashi Matsuo <tmatsuo@google.com>
2021-12-13 12:54:52 -0800

    chore: migrate 10 APIs to owlbot (#4818)
    APIs:
    
    Build
    DataLabeling
    Dataproc
    DataprocMetastore
    Datastore
    DatastoreAdmin
    Debugger
    Deploy
    Dialogflow
    Dlp

--- owlbot.py
+++ owlbot.py
@@ -14,32 +14,25 @@
 
 """This script is used to synthesize generated parts of this library."""
 
-import subprocess
-import synthtool as s
-import synthtool.gcp as gcp
 import logging
-import re
+from pathlib import Path
+import subprocess
+
+import synthtool as s
+from synthtool.languages import php
+from synthtool import _tracked_paths
 
 logging.basicConfig(level=logging.DEBUG)
 
-gapic = gcp.GAPICBazel()
-common = gcp.CommonTemplates()
+src = Path(f"../{php.STAGING_DIR}/DatastoreAdmin").resolve()
+dest = Path().resolve()
 
-library = gapic.php_library(
-    service='datastore',
-    version='v1',
-    bazel_target=f'//google/datastore/admin/v1:google-cloud-datastore-admin-v1-php',
-)
+# Added so that we can pass copy_excludes in the owlbot_main() call
+_tracked_paths.add(src)
 
-# copy all src including partial veneer classes
-s.move(library / 'src')
+php.owlbot_main(src=src, dest=dest)
 
-# copy proto files to src also
-s.move(library / 'proto/src/Google/Cloud/Datastore/Admin', 'src/')
-s.move(library / 'tests/')
 
-# copy GPBMetadata file to metadata
-s.move(library / 'proto/src/GPBMetadata/Google/Datastore/Admin', 'metadata/')
 
 # document and utilize apiEndpoint instead of serviceAddress
 s.replace(
@@ -65,20 +58,6 @@ s.replace(
     """@type string $filter A filtering string.
      *     @type int $pageSize"""
 )
-
-# fix year
-s.replace(
-    '**/Gapic/*GapicClient.php',
-    r'Copyright \d{4}',
-    'Copyright 2020')
-s.replace(
-    '**/V1/DatastoreAdminClient.php',
-    r'Copyright \d{4}',
-    'Copyright 2020')
-s.replace(
-    'tests/**/V1/*Test.php',
-    r'Copyright \d{4}',
-    'Copyright 2020')
 
 ### [START] protoc backwards compatibility fixes
 
